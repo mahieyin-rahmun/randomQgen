ddd8b593079b3f45da3b7a9fd302952061653510
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-02-01 19:52:37 +0600

    backend services for generating pdf buffer array and archiving

---
+++ pages/api/services/archiver.service.ts
@@ -0,0 +1,75 @@
+import archiver, { Archiver, ZipOptions } from "archiver";
+import fs from "fs";
+
+export default class BufferArrayArchiver {
+  private archive: Archiver;
+  private outputFile: fs.WriteStream;
+  private filePath: string;
+  private static instance: BufferArrayArchiver;
+
+  private constructor(
+    private bufferArray: Buffer[],
+    filePath?: string,
+    options: ZipOptions = {
+      zlib: { level: 9 } // Sets the compression level.
+    } 
+  ) {
+    this.filePath = filePath;
+    this.archive = archiver("zip", options);
+    this.outputFile = this.filePath ? fs.createWriteStream(this.filePath) : null;
+    this.setUpListeners();
+
+    if (this.outputFile) {
+      this.archive.pipe(this.outputFile);
+    }
+  }
+  
+  public get Archive() : Archiver {
+    return this.archive;
+  }
+
+  public set FilePath(newFilePath: string) {
+    this.filePath = newFilePath;
+  }
+
+  public set OutputStream(newOutputStream) {
+    this.outputFile = newOutputStream;
+    this.archive.pipe(this.outputFile);
+  }
+
+  private setUpListeners() {
+    this.outputFile?.on("close", () => {
+      console.log(this.archive.pointer() + " bytes written");
+    })
+
+    this.archive.on('warning', (err) => {
+      if (err.code === 'ENOENT') {
+        console.log(err.message);
+      } else {
+        // throw error
+        throw err;
+      }
+    });
+    
+    this.archive.on('error', (err) => {
+      throw err;
+    });
+  }
+
+  getArchivedFile() {
+    for (let index = 0; index < this.bufferArray.length; index++) {
+      const currentBuffer = this.bufferArray[index];
+      this.archive.append(currentBuffer, { name: `Student${index + 1}.pdf` });
+    }
+  }  
+
+  static getBufferArrayArchiver(pdfBufferArray: Buffer[], filePath?: string) {
+    // singleton because I am not sure if the archiver is thread safe   
+    if (this.instance) {
+      return this.instance;
+    }
+
+    this.instance = new BufferArrayArchiver(pdfBufferArray, filePath);
+    return this.instance;
+  }
+}
---
+++ pages/api/services/generate.service.ts
@@ -0,0 +1,44 @@
+import { Fields, Files } from "formidable";
+import fs from "fs";
+import BufferArrayArchiver from "./archiver.service";
+import CombinePdf from "./combinepdf.service";
+import ReconstructQuestionHolder from "./reconstruct.questionholder.service";
+
+interface IFields extends Fields {
+  isMultiPage: string,
+  numberOfStudents: string
+}
+
+export default class GeneratePDFBufferService {
+  constructor(
+    private fields: Fields,
+    private files: Files
+  ) {}
+
+  async generatePdfBufferArray(): Promise<Buffer[]> {
+    const pdfBufferArray = new Array<Buffer>();
+
+    const { isMultiPage, numberOfStudents } = this.fields as IFields;
+    const multiPage: boolean = isMultiPage === "yes";
+    const numberOfStudentsInt = parseInt(numberOfStudents);
+
+    for (let index = 0; index < numberOfStudentsInt; index++) {
+      const reconstructQH = new ReconstructQuestionHolder(multiPage, this.files);
+      const pdfBufferObj = reconstructQH.reconstruct();
+      const combiner = new CombinePdf(pdfBufferObj);
+  
+      let combinedPdfUint8Array: Uint8Array;
+  
+      if (multiPage) {
+        combinedPdfUint8Array = await combiner.combineMultiPageQuestions();
+      } else {
+        combinedPdfUint8Array = await combiner.combineSinglePageQuestions();
+      }
+
+      const combinedPdfBuffer = Buffer.from(combinedPdfUint8Array);
+      pdfBufferArray.push(combinedPdfBuffer);
+    }
+
+    return pdfBufferArray;
+  }
+}
