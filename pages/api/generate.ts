de1929aefcd7cfb178ea1038d7f135bc77d7ec46
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2021-03-02 12:26:42 +0600

    added functionality to create a task folder for the uploaded files and delete it once done

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Fields, Files, IncomingForm } from "formidable";
 import fs from "fs";
+import path from "path";
+import { v4 } from "uuid";
 import GeneratePdfService from "../../services/generate.service";
 import BufferArrayArchiver from "../../services/archiver.service";
 
@@ -30,14 +32,16 @@ const getFieldsAndFiles = (
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
+    const taskId = v4();
+    const rootUploadFolder = path.resolve(process.cwd(), "uploaded");
+    const taskFolder = path.resolve(rootUploadFolder, taskId);
+
     const form = new IncomingForm();
     form.multiples = true;
-    // TODO: change it later
-    form.uploadDir =
-      "/home/phantomhive/Documents/Code/react/randqgen/pages/api/tmp";
+    form.uploadDir = taskFolder;
 
     if (!fs.existsSync(form.uploadDir)) {
-      fs.mkdirSync(form.uploadDir);
+      fs.mkdirSync(form.uploadDir, { recursive: true });
     }
 
     try {
@@ -55,6 +59,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
       const archive = bufferArchiver.Archive;
       await archive.finalize();
+
+      // this doesn't need to be awaited as this is server side clean up
+      fs.rmSync(taskFolder, { recursive: true });
     } catch (err) {
       res.status(500);
       res.end();
